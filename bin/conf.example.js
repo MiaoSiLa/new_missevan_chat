4448026e64b5de2f158f09ac52f6f079919d9bdf
tengattack <tengattack@foxmail.com>
2015-01-15 16:01:22 +0800

    format config

--- bin/conf.example.js
+++ bin/conf.example.js
@@ -17,7 +17,11 @@
 //线下数据
 var config = {
   dev_mode: true,
-  redis: {url:"192.168.1.136",port:"6379",password:"missevan_chat_password_tom" },
+  redis: {
+    url: "192.168.1.136",
+    port: "6379",
+    password: "missevan_chat_password_tom"
+  },
   port: 3000
 };
 
