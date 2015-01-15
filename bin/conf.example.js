4645919dac48968d24f92c0e96956efa9d70ec9a
tengattack <tengattack@foxmail.com>
2015-01-15 15:03:51 +0800

    conf

--- bin/conf.example.js
+++ bin/conf.example.js
@@ -10,6 +10,11 @@
 * 	configure of our application
 */
 
+
+//线上数据
+//module.exports.redis = {url:"10.162.72.20",port:"6379",password:"missevan_chat_password_tom"};
+//module.exports.port = "3000";
+//线下数据
 var config = {
   dev_mode: true,
   redis: {url:"192.168.1.136",port:"6379",password:"missevan_chat_password_tom" },
