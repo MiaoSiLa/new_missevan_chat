97e458c1cf63034aa8c62f49c5e33e62232254b9
tengattack <tengattack@foxmail.com>
2015-01-16 13:03:40 +0800

    add iomiddleware

--- bin/www.js
+++ bin/www.js
@@ -7,7 +7,8 @@ var config = require('./../conf.js'),
 	User = Model.User,
 	Bridge = Model.Bridge;
 
-var co = require('co');
+var ioMiddleware = require('./../lib/iomiddleware'),
+  generator = require('./../lib/generator');
 
 var app = express(),
 	bridge = new Bridge();
@@ -40,128 +41,125 @@ if (config.dev_mode) {
 	io.adapter( socket_redis({pubClient: pub, subClient: sub}) );
 }
 
-function onerror(err) {
+//client连接错误
+client.on("error", function (err) {
   if (err) {
     console.error(err);
   }
-}
-
-//client连接错误
-client.on("error", onerror);
+});
 
 var roomInfo = io.of('/roomInfo');
 var chatRoom = io.of('/chatRoom');
-chatRoom.on('connection', function (socket) {
 
-	//进入房间
-	socket.on("enter room", function() {
-    var so = new Object();
-    so.socket = socket;
-    var onerror = function (err) {
-      socket.emit("errorinfo", e.message);
-    };
-    var args = Array.prototype.slice.call(arguments);
-    args.push(onerror);
-    var mw = function *(info, callback) {
-      if(!((typeof(callback)==='function') || (typeof(info)==='object')))
+var yclient = new generator(client);
+
+function *connection() {
+  var socket = this.socket;
+
+  //进入房间
+  socket.yon("enter room", function *(info, callback) {
+    if ((typeof(callback) !== 'function') || (typeof(info) !== 'object')) {
+      throw new Error("非法参数");
+    }
+
+    var socket = this;
+    //创建房间
+    var roomId = info.roomId;
+    var memberId = info.userId;
+
+    var roomIdInfo = 'room' + roomId + 'Info';
+    var memberIdInfo = 'member' + memberId + 'Info';
+    var roomIdPerson = 'room' + roomId + 'Person';
+
+    client.PERSIST(roomIdInfo);
+    client.PERSIST(memberIdInfo);
+
+    var roomName = yield yclient.HGET(roomIdInfo, 'name');
+    client.SADD("roomNameIndex", roomName);
+
+    socket.roomName = roomName;
+
+    socket.roomId = roomId;
+    socket.broomId = "room"+roomId;
+    socket.join(socket.broomId);
+
+    socket.userId = memberId;
+    socket.buserId = "room"+socket.roomId+"user"+socket.userId;
+    socket.join(socket.buserId);
+
+    var num = yield yclient.HINCRBY(roomIdPerson, memberIdInfo, 1);
+    if (num == 1) {
+      var member = yield yclient.HGETALL(memberIdInfo);
+      if (member) {
+        socket.broadcast.to(socket.broomId).emit('add new member', member);
+        bridge.emit('enter room', {
+          room: socket.roomId,
+          number: '+1',
+          personInfo: member
+        });
+      }
+      client.ZINCRBY('roomIdIndex', 1, roomId);
+    }
+
+    //获取所有在线人员信息
+    var keys = yield yclient.HKEYS(roomIdPerson);
+    var multi = client.multi();
+    keys.forEach(function (key, index) {
+      multi.HGETALL(key);
+    });
+    multi.exec(function (err, userInfos) {
+      callback({ state: true, member: userInfos });
+    });
+
+    //获取在线的message
+    var msgTypes = [1,3,4];
+    for (var i = 0; i < msgTypes.length; i++) {
+      var num = i;
+      var roomIdMessage = 'room' + socket.roomId + 'MessageType' + num;
+      var data = yield yclient.LRANGE(roomIdMessage, 0, -1);
+      var messages = [];
+      data.forEach(function (message) {
+        messages.push(JSON.parse(message));
+      });
+      socket.emit('get message',{ state: true, msg: messages, type: num });
+    }
+  });
+
+  //断开连接
+  socket.on("leaving",function(){
+    try{
+      User.LeaveRoom(socket,bridge,client);
+    }catch(e){
+      socket.emit("errorinfo",e.message);
+    }
+  });
+
+  //离开房间
+  socket.on("disconnect",function(){
+    try{
+      User.LeaveRoom(socket,bridge,client);
+    }catch(e){
+      socket.emit("errorinfo",e.message);
+    }
+  });
+
+  //发送信息
+  socket.on("send message",function (data, callback) {
+    try{
+      if((typeof(callback)!=="function"))
         throw new Error("非法参数");
+      if(!socket.userId)
+        return;
 
-      //创建房间
-      var roomId = info.roomId;
-      var memberId = info.userId;
+      var message = new Message(data);
+      message.sendMessage(socket, client, callback);
+    }catch(e){
+      socket.emit("errorinfo",e.message);
+    }
+  });
+}
 
-      var roomIdInfo = 'room' + roomId + 'Info';
-      var memberIdInfo = 'member' + memberId + 'Info';
-      var roomIdPerson = 'room' + roomId + 'Person';
-
-      client.PERSIST(roomIdInfo);
-      client.PERSIST(memberIdInfo);
-      client.HGET(roomIdInfo,'name',function(err,roomName){
-        client.SADD("roomNameIndex",roomName);
-        socket.roomName = roomName;
-      });
-
-      socket.roomId = roomId;
-      socket.broomId = "room"+roomId;
-      socket.join(socket.broomId);
-
-      socket.userId = memberId;
-      socket.buserId = "room"+socket.roomId+"user"+socket.userId;
-      socket.join(socket.buserId);
-
-      client.HINCRBY(roomIdPerson,memberIdInfo,1,function(err,num){
-        if(num==1){
-          client.HGETALL(memberIdInfo,function(err,member){
-            socket.broadcast.to(socket.broomId).emit("add new member",member);
-            bridge.emit('enter room',{room:socket.roomId,number:"+1",personInfo:member});
-          });
-          client.ZINCRBY('roomIdIndex',1,roomId);
-        }
-      });
-
-      //获取所有在线人员信息
-      client.HKEYS(roomIdPerson,function(err,keys){
-        var multi = client.multi();
-        keys.forEach(function(key,index){
-          multi.HGETALL(key);
-        });
-        multi.exec(function(err,userInfos){
-          callback({state:true,member:userInfos});
-        });
-      });
-
-      //获取在线的message
-      [1,3,4].forEach(function(num,index){
-        var roomIdMessage = "room"+socket.roomId+"MessageType"+num;
-        client.LRANGE(roomIdMessage,0,-1,function(err,data){
-          var messages = [];
-          data.forEach(function(message,index){
-            messages.push(JSON.parse(message));
-          });
-          socket.emit("get message",{state:true,msg:messages,type:num});
-        });
-      });
-    };
-    
-    var fn = co(mw);
-    fn.apply(so, args);
-	});
-
-	//断开连接
-	socket.on("leaving",function(){
-		try{
-			User.LeaveRoom(socket,bridge,client);
-		}catch(e){
-			socket.emit("errorinfo",e.message);
-		}
-	});
-
-	//离开房间
-	socket.on("disconnect",function(){
-		try{
-			User.LeaveRoom(socket,bridge,client);
-		}catch(e){
-			socket.emit("errorinfo",e.message);
-		}
-	});
-
-	//发送信息
-	socket.on("send message",function (data, callback) {
-		try{
-			if((typeof(callback)!=="function"))
-				throw new Error("非法参数");
-			if(!socket.userId)
-				return;
-
-			var message = new Message(data);
-			message.sendMessage(socket, client, callback);
-		}catch(e){
-			socket.emit("errorinfo",e.message);
-		}
-	});
-
-});
-
+chatRoom.on('connection', ioMiddleware(connection));
 
 bridge.on('enter room', function(data){
 	roomInfo.emit('enter room', data);
@@ -170,10 +168,6 @@ bridge.on('enter room', function(data){
 bridge.on('leave room', function(data){
 	roomInfo.emit('leave room', data);
 });
-
-function isEmptyObject(obj) {
-	return !Object.keys(obj).length;
-}
 
 process.on('uncaughtException',function (err) {
 	console.error(err);
