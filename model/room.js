ac8412f1ab9175255041122b101bde2d69d1c46e
Tom Cao <jiepengthegreat@126.com>
2015-01-27 10:20:50 +0800

    添加roomIdIndex

--- model/room.js
+++ model/room.js
@@ -156,7 +156,8 @@ Room.prototype.leave = function *() {
 				});
 				yield messages;
 			}
-
+			
+			yield yclient.ZINCRBY('roomIdIndex', -1, socket.roomId);
 			var userInfo = yield yclient.HGETALL(memberIdInfo);
 			socket.broadcast.to(socket.broomId).emit('leave room', userInfo);
 			this.bridge.emit('leave room', {
