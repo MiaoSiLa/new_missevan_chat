99c71692ef281c5750def6ccba9fe8162fb117d3
tengattack <tengattack@foxmail.com>
2015-02-12 23:41:45 +0800

    update page style insert position

--- controller/gekijou.js
+++ controller/gekijou.js
@@ -9,18 +9,9 @@ var gekijou = new Router();
 
 gekijou.get('/new', function *() {
 
-  var pageStyles = config['app'].dev_mode ? [
-    '/css/gekijou/build/gekijou.css',
-    '/css/gekijou/build/editor.css'
-  ] : [
-    '/css/gekijou/gekijou.min.css',
-    '/css/gekijou/editor.min.css'
-  ];
-
   yield this.render('gekijou/new', {
     title: '创建_小剧场_MissEvan',
-    user: this.user,
-    pageStyles: pageStyles
+    user: this.user
   });
 
 });
